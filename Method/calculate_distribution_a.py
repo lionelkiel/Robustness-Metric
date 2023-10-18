32c858b0bc6d296368753f297cc7f11e20fabd60
Lionel <quantumlionel@gmail.com>
2023-10-18 17:39:49 +0200

    Update / fix functions
    Added upper limit to mu and sample from bins of a instead

--- Method/calculate_distribution_a.py
+++ Method/calculate_distribution_a.py
@@ -3,6 +3,12 @@ import torch
 from torch.distributions.log_normal import LogNormal
 
 
+def log_normal_cdf(x, mu, s):
+    """
+    Log normal cdf, we use this in the integral
+    """
+    return 0.5 * (1 + torch.erf((torch.log(x) - mu) / (s * np.sqrt(2))))
+
 def integrand(thetas, D):
     """
     value under integrand without prior, so integral( val * prior )
@@ -15,39 +21,59 @@ def integrand(thetas, D):
     # For broadcasting of thetas and D we add a dimension to thetas at the end
     thetas = thetas[..., None]
     # thetas is a 2x... tensor, first row is mus, second row is sigmas
-    distributions = LogNormal(thetas[0], thetas[1])
     # D is a 2xN tensor, first row is lower bounds, second row is upper bounds
-    vals = distributions.cdf(D[0]) - distributions.cdf(D[1])
+    vals = log_normal_cdf(D[1],thetas[0],thetas[1]) - log_normal_cdf(D[0],thetas[0],thetas[1])
     # Likelihoods is a tensor of size thetas.shape[1:], for each theta we have a value
     # We aggregate over the last dimension (size N) which is the dimension of D
     Likelihoods = vals.prod(dim=-1)
     return Likelihoods
 
 
-def calculate_s(a, sigma, mu):
-    return (torch.log(a) - mu) / (torch.erfinv(2 * torch.tensor([sigma]) - 1) * np.sqrt(2))
+def common_constant(val):
+    """
+    Common constant when inverting log normal cdf
+
+    :param val: usually P(x<x0) = val
+    """
+    return torch.erfinv(2 * val - 1)
+
+def calculate_mu_upper(a, sigma, p = 0.01, max_val = 0.4):
+    """
+    Calculate the upper bound of mu, this bound is given by the constraint that we know P(x > 0.4) <= 0.01
+    """
+    K = common_constant(1-torch.tensor([p]))/common_constant(sigma)
+    return (K*torch.log(a) - torch.log(torch.tensor([max_val])))/(K-1)
+
+
+def calculate_s(a, mu, sigma):
+    return (torch.log(a) - mu) / (common_constant(sigma) * np.sqrt(2))
 
 
 # We perform monte carlo integration for the integral
-def distribution_a(a, D, sigma, n_samples):
+def distribution_a(a, D, sigma = 0.05, n_samples = 1000):
     """
     We want to find the probability of a given D, to forego computing the integral we use monte carlo integration.
     Since our prior is unigorm for now we just sample uniformly. NOTE: it will be normalized over the range of a which has been given as input
 
-    :param a: a values to query, tensor of size M
+    :param a: a values to query, LxM tensor
     :param D: Data of epsilons, 2xN tensor, first row is lower bounds, second row is upper bounds
     :return:
     """
+    # Turn floats into tensors
+    sigma = torch.tensor([sigma])
+
     # Median of log normal is exp(mu), thus we want to sample exp(mu) uniformly from 0 to 0.4
     # We have seen that mu > log(a) otherwise we cannot get cdf(a) = sigma, thus that is our lower bound
-    mu_upper = np.log(0.4)
-    mus = torch.rand(n_samples, a.shape[0]) * (mu_upper - torch.log(a + 0.001)) + torch.log(a + 0.001)
+    # We give a constraint that P(x > 0.4) <= 0.01, this gives us an upper bound on mu
+    mu_lower = torch.log(a + 0.00001) # We add a small value to a to avoid s = 0
+    mu_upper = calculate_mu_upper(a, sigma)
+    mus = torch.rand(n_samples, *a.shape) * (mu_upper - mu_lower) + mu_lower
 
     ss = calculate_s(a, sigma, mus)
     thetas = torch.stack([mus, ss])
 
     # Monte carlo integration
-    integral_approximation = torch.mean(integrand(thetas, D), dim=0)
+    integral_approximation = torch.mean(integrand(thetas, D), dim=[0,1])
 
     # Normalization
     return integral_approximation / integral_approximation.sum()
