b4150ef40b6d47479491e0fd5e690bf29dae6050
Lionel <quantumlionel@gmail.com>
2024-05-26 18:03:18 +0200

    Getting bounds helperfile

--- Helper/ImportDataset.jl
+++ Helper/ImportDataset.jl
@@ -1,8 +1,5 @@
-using RxInfer
-using Plots
 using CSV
 using DataFrames
-
 
 path = abspath("C:\\Users\\lkiel\\PycharmProjects\\RobustnessMetric\\Datasets\\threshold_distributions_fairness")
 
@@ -14,16 +11,21 @@ df_epsilon = DataFrame(CSV.File(joinpath(path, "df_epsilon_fairness.csv")))
 df_epsilon_crit = DataFrame(CSV.File(joinpath(path, "df_epsilon_crit_fairness.csv")))
 
 Networks = unique(df_epsilon_crit.network)
+test_df = @view df_epsilon[df_epsilon.ds .== "test", :]
 
-for network in Networks:
-    # take only test data
-    df_epsilon_network = df_epsilon[df_epsilon.ds .== "test", :]
+df_bounds = DataFrame(network = [network for network in Networks for _ in unique(test_df[test_df.network .== network, :].image)],
+                      image = [image for network in Networks for image in unique(test_df[test_df.network .== network, :].image)],
+                      lower_bound = 0.0,
+                      upper_bound = 0.4)
 
+
+println("Getting bounds...")
+
+for network in Networks
     # get relevant data for that network
-    df_epsilon_network = df_epsilon_network[df_epsilon_network.network .== network, [:epsilon,:result,:image,:runtime]]
+    df_epsilon_network = test_df[test_df.network .== network, [:epsilon,:result,:image,:runtime]]
 
-    # make a new dataframe with the lower and upper bound for each image
-    df_bounds = DataFrame(image = unique(df_epsilon_network.image), lower_bound = 0.0, upper_bound = 0.4)
+    view_df_bounds = @view df_bounds[df_bounds.network .== network, :]
 
     # get upper and lower bound for each image
     for img in unique(df_epsilon_network.image)
@@ -31,18 +33,18 @@ for network in Networks:
         unsat_epsilons = epsilon_for_image[epsilon_for_image.result .== "unsat", :].epsilon
         sat_epsilons = epsilon_for_image[epsilon_for_image.result .== "sat", :].epsilon
 
-        row_index = findfirst(df_bounds.image .== img)
+        row_index = findfirst(view_df_bounds.image .== img)
 
         if !isempty(unsat_epsilons)
-            df_bounds[row_index, :lower_bound] = maximum(unsat_epsilons)
+            view_df_bounds[row_index, :lower_bound] = maximum(unsat_epsilons)
         else
-            println("empty, unsat", ' ', img)
+            println("empty, unsat", ' ', img, ' ', network)
         end
 
         if !isempty(sat_epsilons)
-            df_bounds[row_index, :upper_bound] = minimum(sat_epsilons)
+            view_df_bounds[row_index, :upper_bound] = minimum(sat_epsilons)
         else
-            println("empty, sat", ' ', img)
+            println("empty, sat", ' ', img, ' ', network)
         end
     end
 end
